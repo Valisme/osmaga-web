b334e3f56c4b599f746861697b53ec2af285f307
Valmero Ahmadtofa <valism3ro@gmail.com>
2023-09-04 15:00:49 +0700

    Add navbar

---
+++ src/components/Navbar.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+import { writable } from "svelte/store";
+import Bars from "./Bars.svelte";
+import Sidebar from "./Sidebar.svelte";
+import X from "./X.svelte";
+let isOpen = false
+
+$: console.log(isOpen)
+
+</script>
+
+<nav class="w-full h-20 lg:w-full lg:h-16 bg-blue-500 flex flex-row justify-end items-center pr-6 lg:pr-0">
+    <ul class="hidden h-full lg:flex lg:flex-row gap-4 justify-start text-xl font-bold px-4 ">
+       <li class="p-4 hover:bg-gray-300/50 rounded-xl">Beranda</li>
+       <li >
+        <div class="group flex flex-col justify-center text-center ">
+            <div class="p-4 hover:bg-gray-300/50 rounded-xl rounded-b-none">Tentang Kami</div>
+            <div class="p-4 invisible group-hover:visible bg-blue-500 z-[999]">Visi & Misi</div>
+            <div class="p-4 rounded-b-xl invisible group-hover:visible bg-blue-500 z-[999]">Struktur Anggota</div>
+        </div>
+       </li> 
+       <li class="p-4 hover:bg-gray-300/50 rounded-xl">Galeri</li> 
+       <li class="p-4 hover:bg-gray-300/50 rounded-xl">Berita</li> 
+       
+    </ul>
+    <button class="block lg:hidden" on:click={()=>isOpen = !isOpen} aria-hidden="true" tab-index="-1"><Bars/></button>
+    {#if isOpen}
+    <Sidebar bind:isOpen={isOpen}/>
+    {/if}
+
+</nav>
---
+++ src/components/Sidebar.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+    export let isOpen:boolean;
+
+</script>
+{#if isOpen}
+<div class="absolute h-full w-full bg-black/50 left-0 top-0" aria-hidden="true" on:click|self={()=>isOpen = !isOpen} >
+    <!-- <button class="absolute right-4 top-4" on:click={()=>isisOpen = !isisOpen}><X/></button> -->
+    <div class="h-full p-4 w-5/6 bg-black  relative">
+        <ul class="text-white text-4xl flex flex-col gap-4">
+            <li>Beranda</li>
+            <li class="ho">
+                <p>Tentang Kami</p>
+            </li>
+            <li>Galeri</li>
+            <li>Blog</li>
+        </ul>
+        <button class="p-2 absolute bottom-8 right-8 bg-green-500 text-xl rounded-lg font-bold" on:click={()=>isOpen = !isOpen}> Tutup Kembali </button>
+    </div>
+</div>
+{/if}
